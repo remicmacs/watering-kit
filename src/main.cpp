55c5baf35a7cd5769e319ca398710c9e6c22546b
remicmacs <rembourgeon@gmail.com>
2020-09-17 10:14:00 +0200

    Add power-saving mode
    Deep sleep routine of 8 secs.
    
    Some blinking routine for debug purposes
    
    Deep sleep can prevent reupload of a program :
    Press reset button when uploading
    When in reset mode, LED should be pulsating on digital pin 13

--- src/main.cpp
+++ src/main.cpp
@@ -1,133 +1,142 @@
 #include <Arduino.h>
-
+#include <avr/wdt.h>
+#include <avr/sleep.h>
 #include <dht.h>
+
 dht DHT;
 #define DHT11_PIN    9
 #define MOISTURE_PIN A2
-#define POTAR_IN A1
+#define VARISTOR_IN A1
 
-int airHumidity;   //environment humidity
-int airTemperature;  // environment temperature
+int airHumidity;
+int airTemperature;
 int soilHumidity;
-int potar;
+int varistor;
 int counter = 0;
 
-#include <avr/wdt.h>
 
-
-#ifndef MYLED
-#define MYLED 12
+#ifndef DEBUGLED
+#define DEBUGLED 12
 #endif
 
 volatile int interrupted = 9;
-volatile int on = 0;
 
-void idleSleep() {
-
-  SMCR = (1<<SE) | (0<<SM2) | (0<<SM1) | (0<<SM0) ;
-}
-
-void pollData(){
-  int chk;
-  chk = DHT.read11(DHT11_PIN);   //Read Data
-  switch (chk){
-    case DHTLIB_OK:
-                // Serial.print("OK,\t");
-                break;
-    case DHTLIB_ERROR_CHECKSUM:
-                // Serial.print("Checksum error,\t");
-                break;
-    case DHTLIB_ERROR_TIMEOUT:
-                // Serial.print("Time out error,\t");
-                break;
-    default:
-                // Serial.print("Unknown error,\t");
-                break;
-  }
-  airHumidity = DHT.humidity;
-  airTemperature = DHT.temperature;
-  soilHumidity = analogRead(MOISTURE_PIN);
-  potar = analogRead(POTAR_IN);
-
-  // Serial.print(F("airHumidity:"));
-  // Serial.print(airHumidity);
-  // Serial.print(F(",\t"));
-  // Serial.print(F("airTemperature:"));
-  // Serial.print(airTemperature);
-  // Serial.print(F(",\t"));
-  // Serial.print(F("Soil Humidity : "));
-  // Serial.print(soilHumidity);
-  // Serial.print(F(",\t"));
-  // Serial.print(F("Potar : "));
-  // Serial.print(potar);
-  // Serial.print(F("\n"));
-  // delay(1000);
-}
-
-void blink(int numberof) {
-  while(numberof--) {
-    digitalWrite(MYLED, HIGH);
-    delay(100);
-    digitalWrite(MYLED, LOW);
-    delay(100);
-  }
-}
-
-void setupBlink() {
-  int nb = 10;
-  while(nb-- != 0) {
-    digitalWrite(MYLED, HIGH);
-    delay(50);
-    digitalWrite(MYLED, LOW);
-    delay(50);
-  }
-}
-
-void turnOn() {
-  digitalWrite(MYLED, HIGH);
-}
-
-void turnOff() {
-  digitalWrite(MYLED, LOW);
-}
-
-void toggle() {
-  if (on) {
-    turnOff();
-    on = 0;
-  } else {
-    turnOn();
-    on = 1;
-  }
-}
-
+/**
+ * Interrupt configured on watchdog vector
+ */
 ISR(WDT_vect) {
-  wdt_reset();
   cli();
+  wdt_reset();
   interrupted = 9;
 }
 
 void watchdogSetup(void){
   cli(); // disable all interrupts
   wdt_reset(); // reset the WDT timer
-  /*   WDTCSR configuration:
-    WDIE  = 1: Interrupt Enable
-    WDE   = 1 :Reset Enable
-  */
+  /**
+   * WDTCSR configuration:
+   * WDIE  = 1: Interrupt Enable
+   * WDE   = 1 :Reset Enable
+   */
   // Enter Watchdog Configuration mode:
   WDTCSR |= (1<<WDCE) | (1<<WDE);
-  // Set Watchdog settings:
+  // Set Watchdog settings: 8s
   WDTCSR = (1<<WDIE) | (1<<WDE) | (1<<WDP3)  | (0<<WDP2) | (0<<WDP1)  | (1<<WDP0);
   sei();
 }
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  // Serial.begin(115200);
-  // Serial.println("SETUP");
-  pinMode(MYLED, OUTPUT);
+/**
+ * Sends the microcontroller to sleep
+ */
+void deepSleep() {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  cli();
+  sleep_enable();
+  sei();
+  sleep_cpu();
+  sleep_disable();
+  sei();
+}
+
+void timedSleep() {
+  watchdogSetup();
+  deepSleep();
+}
+
+void pollBlink() {
+  digitalWrite(DEBUGLED, HIGH);
+  delay(500);
+  digitalWrite(DEBUGLED, LOW);
+  delay(300);
+  digitalWrite(DEBUGLED, HIGH);
+  delay(500);
+  digitalWrite(DEBUGLED, LOW);
+  delay(300);
+}
+
+void interruptBlink() {
+  int n = 3;
+  while(n--) {
+    digitalWrite(DEBUGLED, HIGH);
+    delay(300);
+    digitalWrite(DEBUGLED, LOW);
+    delay(300);
+  }
+}
+
+void setupBlink() {
+  int nb = 10;
+  while(nb-- != 0) {
+    digitalWrite(DEBUGLED, HIGH);
+    delay(50);
+    digitalWrite(DEBUGLED, LOW);
+    delay(50);
+  }
+}
+
+void pollData(){
+  pollBlink();
+  int chk;
+  chk = DHT.read11(DHT11_PIN);   //Read Data
+  switch (chk){
+    case DHTLIB_OK:
+      // Serial.print("OK,\t");
+      break;
+    case DHTLIB_ERROR_CHECKSUM:
+      // Serial.print("Checksum error,\t");
+      break;
+    case DHTLIB_ERROR_TIMEOUT:
+      // Serial.print("Time out error,\t");
+      break;
+    default:
+      // Serial.print("Unknown error,\t");
+      break;
+  }
+  airHumidity = DHT.humidity;
+  airTemperature = DHT.temperature;
+  soilHumidity = analogRead(MOISTURE_PIN);
+  varistor = analogRead(VARISTOR_IN);
+
+  // Serial.print(F("airHumidity:"));
+  // Serial.print(airHumidity);
+  // Serial.print(F(",\t"));
+  // Serial.print(F("airTemperature:"));
+  // Serial.print(airTemperature);
+  // Serial.print(F(",\t"));50
+  // Serial.print(F("Soil Humidity : "));
+  // Serial.print(soilHumidity);
+  // Serial.print(F(",\t"));
+  // Serial.print(F("varistor : "));
+  // Serial.print(varistor);
+  // Serial.print(F("\n"));
+  // delay(1000);
+}
+
+void setup() {
+  // Just to see the setup step
   setupBlink();
+  // initialize LED digital pin as an output.
+  pinMode(DEBUGLED, OUTPUT);
 
   /* Disable the watchdog and wait for more than 2 seconds */
   /* Done so that the Arduino doesn't keep resetting infinitely in case of wrong configuration */
@@ -135,16 +144,12 @@ void setup()
   delay(3000);
 }
 
-void loop()
-{
-
+void loop() {
+  // Signal interrupt
   if (9 == interrupted) {
-    // Serial.println("Interrupted");
-    blink(3);
-    // toggle();
+    interruptBlink();
     interrupted = 0;
-    pollData();
-    watchdogSetup();
-    // idleSleep();
   }
+  pollData();
+  timedSleep();
 }
